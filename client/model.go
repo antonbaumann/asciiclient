a393104060b074baa5aefd603db1c0387a2c16e9
Anton Baumann <antonbaumann4@gmail.com>
2019-07-17 13:00:26 +0200

    add recvCtrl() method

--- client/model.go
+++ client/model.go
@@ -6,54 +6,88 @@ import (
 	"syscall"
 )
 
+const (
+	ClientCtrlPrefix = "C"
+	ServerCtrlPrefix = "S"
+	Protocol         = "GRNVS V:1.0"
+)
+
 type Model struct {
 	Nickname       string
 	Port           int
-	socket         int
+	ctrlSocket     int
+	dataSocket     int
 	token          string
 	sockAddrRemote syscall.Sockaddr
+	buffer         []byte
 }
 
 func New(nickname string) *Model {
 	return &Model{
 		Nickname: nickname,
+		buffer: make([]byte, 2000),
 	}
 }
 
 func (client *Model) Connect(addr string, port int) error {
-	if err := client.createTCPSocket(); err != nil {
-		return err
-	}
+	errMsg := "connect error: %v"
 
 	if err := client.dial(addr, port); err != nil {
-		return err
+		return fmt.Errorf(errMsg, err)
 	}
 
-	err := client.Send(Netstring("C GRNVS V:1.0"))
-	return err
+	protocolMsg := fmt.Sprintf("%v %v", ClientCtrlPrefix, Protocol)
+	if err := client.sendCtrl(Netstring(protocolMsg)); err != nil {
+		return fmt.Errorf(errMsg, err)
+	}
+
+	msg, err := client.recvCtrl()
+	if err != nil {
+		return fmt.Errorf(errMsg, err)
+	}
+
+	fmt.Println(msg)
+
+	return nil
 }
 
-func (client *Model) Send(message string) error {
-	errMsg := "send error: %v"
-	err := syscall.Sendto(client.socket, []byte(message), 0, client.sockAddrRemote)
+func (client *Model) sendCtrl(message string) error {
+	errMsg := "[control] send error: %v"
+	err := syscall.Sendto(client.ctrlSocket, []byte(message), 0, client.sockAddrRemote)
 	if err != nil {
 		return fmt.Errorf(errMsg, err)
 	}
 	return nil
 }
 
-func (client *Model) createTCPSocket() error {
+func (client *Model) recvCtrl() (string, error) {
+	errMsg := "[control] receive error: %v"
+	length, _, err := syscall.Recvfrom(client.ctrlSocket, client.buffer, 0)
+	if err != nil {
+		return "", fmt.Errorf(errMsg, err)
+	}
+	data := client.buffer[:length]
+	return string(data), nil
+}
+
+func (client *Model) createCtrlSocket() error {
 	fd, err := syscall.Socket(syscall.AF_INET6, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return err
 	}
-	client.socket = fd
+	client.ctrlSocket = fd
 	return nil
 }
 
 func (client *Model) dial(addr string, port int) error {
-	errMsg := "dial error: %v"
+	errMsg := "[control] dial error: %v"
 
+	// create a tcp socket
+	if err := client.createCtrlSocket(); err != nil {
+		return fmt.Errorf(errMsg, err)
+	}
+
+	// get ip address from url
 	ipAddr, err := net.ResolveIPAddr("ip6", addr)
 	if err != nil {
 		return fmt.Errorf(errMsg, err)
@@ -69,7 +103,7 @@ func (client *Model) dial(addr string, port int) error {
 		Addr: ipv6,
 	}
 
-	err = syscall.Connect(client.socket, client.sockAddrRemote)
+	err = syscall.Connect(client.ctrlSocket, client.sockAddrRemote)
 	if err != nil {
 		return fmt.Errorf(errMsg, err)
 	}
