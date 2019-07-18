9f74d5213e86bb1f80be21e0af5b361a5acd36c8
Anton Baumann <antonbaumann4@gmail.com>
2019-07-18 20:53:25 +0200

    add argument parser

--- asciiclient.go
+++ asciiclient.go
@@ -3,12 +3,41 @@ package main
 import (
 	"asciiclient/client"
 	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
 )
 
 func main() {
-	asciiClient := client.New("h4ck3rPsch0rr", "2a00:4700:0:9:f::c", 1337)
-	err := asciiClient.SendString("m")
-	if err != nil {
-		fmt.Println(err)
+	app := cli.NewApp()
+	app.Name = "asciiclient"
+	app.Usage = "./asciiclient -m <message> <nick> <destination>"
+
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "message, m",
+			Value: "",
+			Usage: "message string",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println(c.Args())
+		if len(c.String("message")) == 0 {
+			log.Fatal("tried to send empty string")
+		}
+		asciiClient := client.New(
+			c.Args().Get(0),
+			c.Args().Get(1),
+			1337)
+		err := asciiClient.SendString(c.String("message"))
+		if err != nil {
+			fmt.Println(err)
+		}
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
